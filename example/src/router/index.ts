e1b15b05e76312ce00bef50f5520ccafd72bf60f
LiHowe <howe.personal@gmail.com>
2022-01-06 16:06:34 +0800

    update: update the example case

--- example/src/router/index.ts
+++ example/src/router/index.ts
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import HelloWorld from '../views/HelloWorld.vue'
 export const routes = [
   {
     path: '/',
     name: '首页',
-    component: () => import('../views/HelloWorld.vue')
+    component: HelloWorld
   },
   {
     path: '/demo',
@@ -15,6 +15,21 @@ export const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from) => {
+  console.log('enter BeforeEach', { to, from })
+  return true
+})
+
+router.afterEach((to, from) => {
+  console.log('enter afterEach', { to, from })
+  return true
+})
+
+router.beforeResolve(to => {
+  console.log('enter beforeResolve', { to })
+  return true
 })
 
 export default router
--- example/src/views/Demo.vue
+++ example/src/views/Demo.vue
@@ -1,5 +1,21 @@
 <template>
   This is demo page
 </template>
-<script setup>
+
+<script lang="ts">
+export default {
+  beforeRouteEnter() {
+    console.log('[Demo] beforeRouteEnter')
+  },
+  beforeRouteUpdate() {
+    console.log('[Demo] beforeRouteUpdate')
+  },
+  beforeCreate() {
+    console.log('[Demo] beforeCreate')
+  },
+  created() {
+    console.log('[Demo] created')
+
+  }
+}
 </script>
